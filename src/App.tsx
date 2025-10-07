fa241fa4ac858a25f10b69a2cdbad9ccbbfd7141
Stephen Tersoo <stephentersoo17@gmail.com>
2025-10-07 22:23:54 +0100

    Initialized the course form project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import RegistrationForm from "./RegistrationForm";
+
+function App() {
+    return (
+        <>
+            <div className="app-container">
+                <div className="app-header">
+                    <h1 className="app-title">Speckledot Tech Academy</h1>
+                    <p className="app-subtitle">
+                        Enroll in our premium online courses
+                    </p>
+                </div>
+                <RegistrationForm />
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/RegistrationForm.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function RegistrationForm() {
+    return <div className="form-wrapper"></div>;
+}
+
+export default RegistrationForm;
